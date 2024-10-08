ae3d7ad947fdfa32ed40c7957cb6f464c7ef2159
antonioumssgit <201604455@est.umss.edu>
2024-10-07 20:44:26 -0400

    creacion del navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './componets/navbar/Navbar';
 import { Recorridos } from './recorridos/Recorridos';
 
 function App() {
   return (
     <div className = "containerApp">
+      <Navbar/>
       <Recorridos/>
     </div>
   );
---
+++ frontend/src/componets/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';  // Archivo CSS para los estilos
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">Mi Proyecto</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
